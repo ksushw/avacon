4bf301f28a6e77e0e6e8e71cc7d8f1af7eb16aa6
Nevarenyh Kseniya <xenianevarenyh@yandex.ru>
2024-06-19 22:53:45 +0500

    try

--- src/composables/useAxios.ts
+++ src/composables/useAxios.ts
@@ -15,7 +15,7 @@ export async function useAxios<T>(url: string, options: AxiosRequestConfig = {})
   const defaultOptions: AxiosRequestConfig = {
     url,
     method: 'GET',
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: 'https://avacoin.elastoo.com/api',
     // @ts-ignore
     transformResponse: [function (data, headers, status) {
       console.log(`%c ${this.method?.toUpperCase()} ${url}`, `background: green`, JSON.parse(data))
